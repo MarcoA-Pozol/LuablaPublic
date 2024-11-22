bcce49041223aaf7ed4dd101b8d3d8e975987063
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2024-11-21 18:24:18 -0600

    First Landing Page Version}: 2.7

--- Authentication/models.py
+++ Authentication/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class User(AbstractUser):
     """All Django builded-in User model fields will be inherited to this model and additionally it will have another custom fields"""
     
-    confirm_password = models.CharField(max_length=20, null=False)
     personal_description = models.TextField(null=True, default="I am learning a new language!")
     age = models.IntegerField(null=True, default=0)
     genre = models.CharField(max_length=50, null=True, default="Confident")
@@ -13,6 +12,8 @@ class User(AbstractUser):
     profile_picture = models.ImageField(upload_to="profile_pictures/", null=True, unique=False, default="profile_pictures/default_profile_picture.jpg")
     score = models.IntegerField(null=False, default=0)
     friends = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='friends_with')
+    # App flow fields
+    language_picked = models.BooleanField(null=True, default=False)
 
     def __str__(self):
         return self.username
--- Welcome/views.py
+++ Welcome/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from Authentication.models import User
 
 def welcome(request):
     return render(request, 'home.html')
@@ -7,11 +8,20 @@ def about(request):
     return render(request, 'about.html')
 
 def languages_selection(request):
-    return render(request, 'languages_selection.html')
+    # Auth user
+    user = User.objects.get(username=request.user)
+    if user.language_picked == False:
+        return render(request, 'languages_selection.html')
+    else:
+        return redirect('study')
 
 def ACTION_select_language(request):
+    # Auth user
+    user = User.objects.get(username=request.user)
     if request.method == 'POST':
         selected_language = request.POST.get('language')
         request.session['selected_language'] = selected_language
+        user.language_picked = True
+        user.save()
         return redirect('study') 
     return redirect('study')
